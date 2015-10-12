a341301e8c2c83b82cb6991d7c64592972239b33
Doug Shamoo <doug.shamoo@gmail.com>
2015-10-12 12:53:49 -0700

    (fix) Fixed discover data initialization.

--- client/app/discover/discover.js
+++ client/app/discover/discover.js
@@ -1,8 +1,16 @@
 angular.module('jupitr.discover', [])
 
 .controller('discoverController', function($scope, User) {
+
+  $scope.profiles = [];
+
+  User.getAll(function(data) {
+    $scope.profiles = data;
+    // console.log(data);
+    // console.log($scope.profiles);
+  });
   
   // retrieves all user profiles from local storage to display on discover page
-  $scope.profiles = JSON.parse(window.localStorage.getItem('hrr8.jupitr'));
+  // $scope.profiles = JSON.parse(window.localStorage.getItem('hrr8.jupitr'));
   
 });
