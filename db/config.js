6129975e158ea8e156fc0c399ebc494d12b9c01d
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-10-12 12:38:57 -0600

    More minor fixes

--- db/config.js
+++ db/config.js
@@ -15,13 +15,3 @@ db.on('diconnected', mongoose.connect);
 
 module.exports = db;
 
-module.exports = {
-  github: {
-    appId: '0d89f243fcd157e1907c',
-    appSecret: '651f673086cee00cedd98a4830c4c45bfd70014f'
-  },
-  linkedin: {
-    appId: '78nkitgng9t2p3',
-    appSecret: 'hGF4gZlBNghdXZS4'
-  }
-};
